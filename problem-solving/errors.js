b4b15c2953fd9da2d210bb003237eb4bf2523642
Christian Dutton <Dutton1990@googlemail.com>
2022-04-01 17:01:31 +0100

    add relabelling

--- problem-solving/errors.js
+++ problem-solving/errors.js
@@ -1,22 +1,30 @@
 // Work through these challenges in order, regularly running the tests until they all pass!
 
+// TASK 1  
 function getFirstItem(arr) {
+  // getFirstItem returns the first item in the array
   const firstItem = arr[0];
 
   return firstitem;
 }
 
+//TASK 2 
 function splitString() {
+  // splitString separates a string into letters and returns it as an array
   return string.split('');
 }
 
+// TASK3
 function addBread(person, bread) {
+  // addBread adds a favourite bread to the person object and returns the updated object
   person['loaf'] = 'sourdough';
 
   return;
 }
 
+//TASK 4
 function countTheChars(array, char) {
+  // countTheChars counts the occurrences of the char in the array and returns the count
   const count = 0;
 
   return count;
@@ -27,7 +35,11 @@ function countTheChars(array, char) {
   }
 }
 
+//TASK 5
 function addGuestsToParty(peopleArray) {
+  // addGuestsToParty looks for anyone in the peopleArray who have RSVP's yes 
+  // and adds them to the guests array in the party object. 
+  // It then returns the guest array.
   const party = {
     host: 'Paul Copley',
     venue: 'Hatch',
