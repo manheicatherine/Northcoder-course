57338a9db715f63344c29fa4cd024f74a7737a90
Poonam <p_rajput@hotmail.co.uk>
2022-04-01 10:21:50 +0100

    adding some more refactor katas

--- __tests__/refactoring.test.js
+++ __tests__/refactoring.test.js
@@ -1,4 +1,20 @@
-const { isSweetEnough } = require('../problem-solving/refactoring');
+const {
+  isSweetEnough,
+  shoutNames,
+  isShrekCharacter,
+} = require('../problem-solving/refactoring');
+
+describe('shoutNames', () => {
+  it('returns all names uppercased', () => {
+    let names = ['Carrie', 'Diya', 'Kyle', 'Christian'];
+    let expected = ['CARRIE', 'DIYA', 'KYLE', 'CHRISTIAN'];
+    expect(shoutNames(names)).toEqual(expected);
+
+    names = ['Maddie', 'Bethan', 'Andrea', 'Jade'];
+    expected = ['MADDIE', 'BETHAN', 'ANDREA', 'JADE'];
+    expect(shoutNames(names)).toEqual(expected);
+  });
+});
 
 describe('isSweetEnough', () => {
   it('returns true when all foods are sweet', () => {
@@ -10,19 +26,95 @@ describe('isSweetEnough', () => {
     expect(isSweetEnough(foodArray)).toBe(true);
   });
   it('returns false when no foods are sweet', () => {
-      const foodArray = [
-          {name: 'samosa', flavour: 'savoury'},
-          {name: 'lemon', flavour: 'sour'},
-          {name: 'olive', flavour: 'bitter'}
-      ]
-      expect(isSweetEnough(foodArray)).toBe(false);
+    const foodArray = [
+      { name: 'samosa', flavour: 'savoury' },
+      { name: 'lemon', flavour: 'sour' },
+      { name: 'olive', flavour: 'bitter' },
+    ];
+    expect(isSweetEnough(foodArray)).toBe(false);
   });
   it('returns false when not all foods are sweet', () => {
     const foodArray = [
-        {name: 'stollen', flavour: 'sweet'},
-        {name: 'cranberries', flavour: 'sour'},
-        {name: 'mince pie', flavour: 'sweet'}
-    ]
+      { name: 'stollen', flavour: 'sweet' },
+      { name: 'cranberries', flavour: 'sour' },
+      { name: 'mince pie', flavour: 'sweet' },
+    ];
     expect(isSweetEnough(foodArray)).toBe(false);
+  });
 });
+
+describe('isShrekCharacter', () => {
+  it('returns empty array if no characters are from the movie, Shrek.', () => {
+    const characters = [
+      { name: 'Cedric Diggory', movie: 'Harry Potter and the Goblet of Fire' },
+      { name: 'Elle Woods', movie: 'Legally Blonde' },
+      { name: 'Paddington Bear', movie: 'Paddington 2' },
+    ];
+
+    expect(isShrekCharacter(characters)).toEqual([]);
+  });
+  it('returns all characters if all characters are from the movie, Shrek.', () => {
+    const characters = [
+      {
+        name: 'Shrek',
+        movie: 'Shrek',
+      },
+      { name: 'Lord Farquaad', movie: 'Shrek' },
+      { name: 'Magic Mirror', movie: 'Shrek' },
+    ];
+
+    expect(isShrekCharacter(characters)).toEqual([
+      {
+        name: 'Shrek',
+        movie: 'Shrek',
+      },
+      { name: 'Lord Farquaad', movie: 'Shrek' },
+      { name: 'Magic Mirror', movie: 'Shrek' },
+    ]);
+  });
+  it('returns some characters if some characters are from the movie, Shrek.', () => {
+    const characters = [
+      {
+        name: 'Cher',
+        movie: 'Clueless',
+      },
+      {
+        name: 'Hans Gruber',
+        movie: 'Die Hard',
+      },
+      {
+        name: 'Donkey',
+        movie: 'Shrek',
+      },
+      {
+        name: 'Lola',
+        movie: 'Shark Tale',
+      },
+    ];
+    const expected = [
+      {
+        name: 'Donkey',
+        movie: 'Shrek',
+      },
+    ];
+
+    expect(isShrekCharacter(characters)).toEqual(expected);
+  });
+  it('returns any characters from the Shrek franchise.', () => {
+    const characters = [
+      { name: 'Fiona', movie: 'Shrek' },
+      { name: 'Fairy Godmother', movie: 'Shrek 2' },
+      { name: 'Cookie', movie: 'Shrek 4' },
+      { name: 'Puss In Boots', movie: 'Shrek 2' },
+      { name: 'Han Solo', movie: 'Star Wars: A New Hope' },
+    ];
+    const expected = [
+      { name: 'Fiona', movie: 'Shrek' },
+      { name: 'Fairy Godmother', movie: 'Shrek 2' },
+      { name: 'Cookie', movie: 'Shrek 4' },
+      { name: 'Puss In Boots', movie: 'Shrek 2' },
+    ];
+
+    expect(isShrekCharacter(characters)).toEqual(expected);
+  });
 });
--- problem-solving/refactoring.js
+++ problem-solving/refactoring.js
@@ -1,7 +1,21 @@
+// TASK 1
+// Refactor shoutNames to use the higher order function, map, instead of the for loop. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+function shoutNames(names) {
+  // shoutNames capitalises all the names and returns them in a new array.
+  const shoutyArray = [];
+  for (let i = 0; i < names.length; i++) {
+    const upperCaseName = names[i].toUpperCase();
+    shoutyArray.push(upperCaseName);
+  }
+  return shoutyArray;
+}
+
+// TASK 2
 // Refactor isSweetEnough to use the higher order function, every. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 
 function isSweetEnough(foodArray) {
-  // this function returns true if ALL foods are sweet
+  // isSweetEnough returns true if ALL foods are sweet.
   for (let i = 0; i < foodArray.length; i++) {
     if (foodArray[i].flavour !== 'sweet') {
       return false;
@@ -10,15 +24,22 @@ function isSweetEnough(foodArray) {
   return true;
 }
 
-/*
+// TASK 3
+// Refactor isShrekCharacter to use a higher order function and tidy up the code.
 
-    x 3
+function isShrekCharacter(characters) {
+  // isShrekCharacter receives an array of characters and *only* returns the characters from the Shrek franchise in a new array.
+  console.log(characters);
+  const x = [];
 
-    katas already written with lower level method (passing tests) -> refactor this to use map / filter
-    bad variable names 
-    unnecessary variables
+  for (let i = 0; i < characters.length; i++) {
+    let character = characters[i];
+    if (characters[i].movie.includes('Shrek')) {
+      x.push(characters[i]);
+    }
+  }
+  return x;
+}
 
-    eventually refactor this to use higher order method (point to docs)
-
-*/
-module.exports = { isSweetEnough };
+// NOTE: Remember to remove any unused variables or console logs from the code.
+module.exports = { isSweetEnough, shoutNames, isShrekCharacter };
