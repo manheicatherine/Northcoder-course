44a5a7603b5670f8ad828bc0c505047acd59a8bc
Poonam <p_rajput@hotmail.co.uk>
2022-04-05 13:09:22 +0100

    separating a describe block

--- __tests__/maths.test.js
+++ __tests__/maths.test.js
@@ -9,24 +9,26 @@ const {
   splitTheBeans,
 } = require('../challenges/katas/1-maths');
 
-describe('Basic Maths', () => {
+describe('modulo', () => {
   it('modulo returns the remainder of dividing a by b', () => {
     expect(modulo(4, 2)).toEqual(0);
     expect(modulo(7, 2)).toEqual(1);
   });
+});
 
+describe('squareRoot', () => {
   it('squareRoot returns the square root of n', () => {
     expect(squareRoot(4)).toEqual(2);
     expect(squareRoot(16)).toEqual(4);
   });
-
+});
+describe('raiseToPower', () => {
   it('raiseToPower returns m to the nth power§', () => {
     expect(raiseToPower(2, 3)).toEqual(8);
     expect(raiseToPower(3, 4)).toEqual(81);
     expect(raiseToPower(8, 0)).toEqual(1);
   });
 });
-
 describe('formatMoney', () => {
   it('adds zeroes up to 2 decimal numbers', () => {
     expect(formatMoney(1)).toEqual('£1.00');
