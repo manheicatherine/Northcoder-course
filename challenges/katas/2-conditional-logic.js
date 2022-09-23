ebac70c5177457d18e5e26936b3e5f29a5e22101
Catherine Li <Catherinelization@Catherinede-MacBook-Air.local>
2022-09-23 18:46:52 +0100

    completed conditional-logic test

--- challenges/katas/2-conditional-logic.js
+++ challenges/katas/2-conditional-logic.js
@@ -3,22 +3,48 @@
 
 function areValuesEqual(left, right) {
   // return true if the passed arguments are strictly equal
+  if(left === right){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function areValuesNotEqual(left, right) {
   // return true if the passed arguments are NOT equal
+  if(left !== right){
+    return true;
+  }
+  return false;
 }
 
 function isFromThe60s(year) {
   // return true if the year is in the 1960's
+  if(year >= 1960 && year <= 1969){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isEvenLength(string) {
   // return true if string has an even length
+  if(string.length % 2 === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function areStringsEqualCaseInsensitive(left, right) {
   // return true if the passed strings are equal ignoring case
+  //hello HELLO
+  //hello hello
+  if(left.toLowerCase() === right.toLowerCase()){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // ---------- Do not change the code below this line --------------
