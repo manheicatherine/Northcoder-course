a6c74a940847d3a61f68731b47f56a2cfef0387c
Catherine Li <Catherinelization@Catherinede-MacBook-Air.local>
2022-09-27 18:11:15 +0100

    completed strings test

--- challenges/katas/3-strings.js
+++ challenges/katas/3-strings.js
@@ -3,37 +3,62 @@
 
 function getLength(str) {
   // return the length of the passed string
+  return str.length;
 }
 
 function putNamesTogether(firstName, lastName) {
-  // return the first and last names with a space between them
+  // return the first and last names with a space between them 
+  // return `${firstName} ${lastName}`
+  return firstName + " " +lastName;
 }
 
 function capitaliseString(str) {
   // return the passed string with its first letter capitalised
+  //`${str[0].toUpperCase()}${str.slice(1)}`
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
 
 function dontShoutSentence(SENTENCE) {
   // return the passed sentence (IN ALL CAPS) in lower case
   // keep the first character capitalised
+  // const newSen = SENTENCE.toLowerCase();
+  // return newSen[0].toUpperCase() + newSen.slice(1);
+  return SENTENCE[0].toUpperCase() + SENTENCE.slice(1).toLowerCase();
+
 }
 
 function getMiddle(str) {
   // return the middle (or middle two) character(s) of the passed string
+  if(str.length % 2 !== 0 ){
+    // console.log(str[Math.ceil(str.length / 2)])
+    return str[Math.floor(str.length / 2)];
+  } else if (str.length % 2 === 0){
+    return str[Math.round(str.length / 2)] + str[Math.ceil(str.length / 2)];
+  }
 }
 
 function getLastWord(words) {
   // return the last word of a sentence
   // you can assume words doesn't have a period at the end
+  const arrWords =  words.split(" ");
+  return arrWords[arrWords.length - 1];
 }
 
 function hyphenateWords(words) {
   // return the passed string with the spaces replaced by hyphens ("-")
+  // return words.split(" ").join("-");
+  return words.replace(/ /g, "-");
 }
 
 function convertToCamelCase(words) {
   // convert the passed string of space-separated words to camel case
   // camel case looks like this --> camelCaseLooksLikeThis
+  const arrWords =  words.split(" ").map(newWords => {
+    return newWords[0].toUpperCase()+ newWords.slice(1).toLowerCase()
+  });
+  arrWords[0] =   arrWords[0].toLowerCase();
+  return arrWords.join("");
 }
 
 function passwordValidation(password) {
@@ -46,6 +71,19 @@ function passwordValidation(password) {
   You need to design this function to check the password it is passed. If it fits all of the above criteria then you should return 'valid' else return 'invalid'.
 
 */
+
+  // if(password.indexOf(/n/i) !== -1 && password.indexOf(/c/i) !== -1 && password[password.length - 1] === "1"){
+  //   return "valid";
+  // } else {
+  //   return "invalid";
+  // }
+    const passwordArr = password.toLowerCase()
+    const passArr = passwordArr.split('');
+    if (passArr.includes("n") && passArr.includes("c") && passArr[passArr.length -1] == 1) {
+      return "valid";
+    } else {
+    return "invalid";
+    }
 }
 
 // ---------- Do not change the code below this line --------------
