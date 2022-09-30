1878efdd9e92977639d7acd123f8506e7de26cbb
Catherine Li <Catherinelization@catherinede-air.home>
2022-09-30 15:24:04 +0100

    completed errors test

--- problem-solving/errors.js
+++ problem-solving/errors.js
@@ -4,35 +4,42 @@
 function getFirstItem(arr) {
   // getFirstItem returns the first item in the array
   const firstItem = arr[0];
-
-  return firstitem;
+  return firstItem;
 }
 
 //TASK 2
-function splitString() {
+function splitString(str) {
   // splitString separates a string into letters and returns it as an array
-  return string.split('');
+  
+  // return arguments.split('');
+  return str.split('');
+  
 }
 
 // TASK3
 function addBread(person, bread) {
   // addBread adds a favourite bread to the person object and returns the updated object
-  person['loaf'] = 'sourdough';
+  // person['loaf'] = 'sourdough';
+  if(person["name"] === 'Vel'){
+    person['faveBread'] ='sourdough';
+  } else if(person["name"] === 'Paul'){
+    person['faveBread'] = 'tiger loaf';
+  }
 
-  return;
+  return person;
+
 }
 
 //TASK 4
 function countTheChars(array, char) {
   // countTheChars counts the occurrences of the char in the array and returns the count
-  const count = 0;
-
-  return count;
-  for (let i = 0; i < array; i++) {
-    if (array[i] == char) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === char) {
+      count++;
     }
-    count++;
   }
+  return count;
 }
 
 //TASK 5
@@ -48,12 +55,12 @@ function addGuestsToParty(peopleArray) {
   };
 
   peopleArray.forEach((person) => {
-    if (person.RSVP) {
-      party.push({ name: person.name });
+    if (person["RSVP"] === 'yes') {
+      party["guests"].push({ name: person["name"] });
     }
   });
 
-  return party;
+  return  party["guests"];
 }
 
 // ---------- Do not change the code below this line --------------
